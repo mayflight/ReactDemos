d23e19050947967bc4568ad6e470f238dc6e42d5
mayflight <18868824465@163.com>
2017-02-22 14:26:25 +0800

    combineReducers

--- src/myFunction.js
+++ src/myFunction.js
@@ -1,12 +1,21 @@
 
  const changeSize = (state=10,action) => {
+ 	if(action.type === 'RESET') {
+ 		return 10
+ 	 }
  	return action.type === 'SIZE' ? state+ action.value: state
  }
  const changeHeight= (state=100,action) => {
+ 	if(action.type === 'RESET') {
+ 		return 100
+ 	 }
  	return action.type === 'HEIGHT' ? state+ action.value: state
  }
 
  const changeWidth= (state=100,action) => {
+ 	if(action.type === 'RESET') {
+ 		return 100
+ 	 }
  	return action.type === 'WIDTH' ? state+ action.value: state
  }
 
